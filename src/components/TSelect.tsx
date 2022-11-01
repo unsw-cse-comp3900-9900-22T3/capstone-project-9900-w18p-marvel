b5307da8a5610e89bea1e791b4bfb57bd46909bd
zhihui-shen <zhihui.shen@student.unsw.edu.au>
2022-11-02 00:11:15 +1100

    role

--- src/components/TSelect.tsx
+++ src/components/TSelect.tsx
@@ -1,17 +1,18 @@
 import * as React from "react";
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { MenuItem } from "@mui/material";
-interface Props{
-  defaultValue:string
-  onChange?:(val:string)=>void
+interface Props {
+  defaultValue: string;
+  onChange?: (val: string) => void;
+  values: Array<string>;
 }
 
-export default function TSelect({defaultValue,onChange}:Props) {
+export default function TSelect({ defaultValue, onChange, values=[] }: Props) {
   const [status, setStatus] = React.useState("");
 
   const handleChange = (event: any) => {
     setStatus(event.target.value);
-    onChange?.(event.target.value)
+    onChange?.(event.target.value);
   };
 
   return (
@@ -21,9 +22,62 @@ export default function TSelect({defaultValue,onChange}:Props) {
       defaultValue={defaultValue}
       variant="filled"
       onChange={handleChange}
+      sx={{
+        width: "102px",
+        height: "40px",
+        borderRadius: "16px",
+        "&.MuiSelect-select": {
+          borderRadius: "9999px",
+          paddingLeft: "32px",
+          padding: "0px",
+        },
+        "&.MuiSelect-filled": {
+          backgroundColor: "transparent",
+        },
+        "&.MuiInputBase-input": {
+          backgroundColor: "transparent",
+          fontWeight:700,
+        },
+        "&.MuiFilledInput-input": {
+          // backgroundColor:'transparent',
+          fontWeight:700,
+        },
+        "&.MuiInputBase-root": {
+          backgroundColor:'rgba(143, 146, 161, 0.1)',
+          // display:'none'
+        },
+        "&.MuiFilledInput-root": {
+          // backgroundColor:'transparent',
+          // display:'none'
+        },
+        "&.MuiFilledInput-underline": {
+          // backgroundColor:'transparent',
+        },
+        "&::before": {
+          display: "none",
+        },
+        "&::after": {
+          display: "none",
+        },
+        "&.MuiSelect-iconFilled": {
+          color: "#ffffff",
+          backgroundColor: "#ffffff",
+          display: "none",
+        },
+        "&.MuiSvgIcon-root": {
+          color: "#ffffff",
+          backgroundColor: "#ffffff",
+          display: "none",
+        },
+        "& svg path": {
+          fill: "rgba(0, 0, 0, 0.2)",
+          backgroundColor: "#ffffff",
+        },
+      }}
     >
-      <MenuItem  value="ongoing">In Progress</MenuItem >
-      <MenuItem  value="done">Complete</MenuItem >
+      {values.map((v) => (
+        <MenuItem value={v}>{v}</MenuItem>
+      ))}
     </Select>
   );
 }

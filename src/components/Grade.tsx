f48ed1539b9180d4807ee8267f9d20c5e9921117
cyberyuan0817 <107191848+cyberyuan0817@users.noreply.github.com>
2022-11-16 12:47:55 +1100

    Update Grade.tsx

--- src/components/Grade.tsx
+++ src/components/Grade.tsx
@@ -19,7 +19,7 @@ export const Grade = ({ id, onComplete }: GradeProp) => {
 
     return (
         <div className="flex flex-col w-72 bg-white-100 rounded-[32px]" >
-            <div className="text-3xl w-72 text-center h-12 text-gray-500 pt-5">Grade</div>
+            <div className="text-3xl w-72 text-center h-12 text-gray-500 pt-5">Grade it!</div>
 
             <div className="flex flex-row w-72 justify-between h-12 mt-4">
                 <div className="ml-7 leading-10 text-2xl text-blue-700">Perfect</div>
